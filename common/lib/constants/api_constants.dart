38b85754125de2bbc7c79c44f289418b809b3fbe
PinzaruDaniel <pinzaru.danieel@gmail.com>
2025-07-24 12:07:19 +0300

    Added submodules

---
+++ common/lib/constants/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants{
+  static const String baseUrl='https://api.ecommerce.ebsio.dev.devebs.net';
+  static const Map<String, String> saasAppToken  = {'SAAS-APP-TOKEN': '51b5c181d7d441029a8d03c7689dbde7'};
+}
