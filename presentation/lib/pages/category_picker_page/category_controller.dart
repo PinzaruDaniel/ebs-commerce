4077b0ddd875790e5c6cb09c791b2db8cee20f36
PinzaruDaniel <pinzaru.danieel@gmail.com>
2025-08-07 11:36:27 +0300

    am facut pagina cu categorii

--- presentation/lib/pages/category_picker_page/category_controller.dart
+++ presentation/lib/pages/category_picker_page/category_controller.dart
@@ -9,6 +9,7 @@ class CategoryController extends GetxController {
   final GetAllCategoriesUseCase getAllCategoriesUseCase = GetIt.instance<GetAllCategoriesUseCase>();
 
   RxList<CategoryViewModel> categories = RxList([]);
+  RxMap<int?, List<CategoryViewModel>> groupedCategories=<int?, List<CategoryViewModel>>{}.obs;
   @override
   void onInit(){
     super.onInit();
@@ -18,8 +19,17 @@ class CategoryController extends GetxController {
   void getCategories() async {
     await getAllCategoriesUseCase.call().then((either) async {
       either.fold((failure) {}, (categories) async {
-        this.categories.value = categories.map((e) => e.toModel).toList();
+        final models = categories.map((e) => e.toModel).toList();
+        this.categories.value=models;
+        groupedCategories.value=groupByParent(models);
       });
     });
+  }
+  Map<int?, List<CategoryViewModel>> groupByParent(List<CategoryViewModel> categories){
+    final Map<int?, List<CategoryViewModel>>map={};
+    for(var cat in categories){
+      map.putIfAbsent(cat.parent, ()=>[]).add(cat);
+    }
+    return map;
   }
 }
--- presentation/lib/pages/category_picker_page/category_picker_page.dart
+++ presentation/lib/pages/category_picker_page/category_picker_page.dart
@@ -4,6 +4,7 @@ import 'package:presentation/pages/filtered_page/filter_controller.dart';
 import 'package:presentation/util/resources/app_icons.dart';
 import 'package:presentation/util/widgets/app_bar_widget.dart';
 import 'package:presentation/util/widgets/bottom_navigation_bar_widget.dart';
+import 'package:presentation/util/widgets/circular_progress_indicator_page_widget.dart';
 import 'package:presentation/view/product_view_model.dart';
 import '../../util/resources/app_colors.dart';
 import '../../util/resources/app_text_styles.dart';
@@ -38,6 +39,98 @@ class _CategoryPickerPageState extends State<CategoryPickerPage> {
         actions: [IconButton(onPressed: AppRouter.openShoppingCartPage, icon: AppIcons.cartIcon)],
       ),
       body: ListView.builder(
+        itemCount: catController.groupedCategories[null]?.length,
+        itemBuilder: (context, index) {
+          final parent = catController.groupedCategories[null]![index];
+          final hasChildren = catController.groupedCategories.containsKey(parent.id);
+          return Obx(() {
+            final selected = filController.selectedCategoryId.contains(parent.id);
+            return ExpansionTile(
+              shape: Border(),
+              iconColor:  hasChildren? AppColors.primary: Colors.transparent,
+              collapsedIconColor: hasChildren? Colors.black: Colors.transparent,
+              title: Row(
+                children: [
+                  Checkbox(
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                    side: BorderSide(color: Colors.grey.shade300, width: 2),
+                    value: selected,
+                    onChanged: (v) => filController.toggleCategory(parent.id, v ?? false),
+                    activeColor: AppColors.primary,
+                  ),
+                  Expanded(child: Text(parent.name, style: AppTextsStyle.bold())),
+                ],
+              ),
+
+              children: hasChildren
+                  ? List.generate(catController.groupedCategories[parent.id]!.length, (childIndex) {
+                      final child = catController.groupedCategories[parent.id]![childIndex];
+                      final hasGrandChildren = catController.groupedCategories.containsKey(child.id);
+                      final selectedChild = filController.selectedCategoryId.contains(child.id);
+                      return hasGrandChildren
+                          ? ExpansionTile(
+                              shape: Border(),
+                              iconColor: AppColors.primary,
+                              title: Padding(
+                                padding: const EdgeInsets.only(left: 16.0),
+                                child: Row(
+                                  children: [
+                                    Checkbox(
+                                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                                      side: BorderSide(color: Colors.grey.shade300, width: 2),
+                                      value: selectedChild,
+                                      onChanged: (v) => filController.toggleCategory(child.id, v ?? false),
+                                      activeColor: AppColors.primary,
+                                    ),
+                                    Text(child.name, style: AppTextsStyle.medium),
+                                  ],
+                                ),
+                              ),
+                              children: List.generate(catController.groupedCategories[child.id]!.length, (grandIndex) {
+                                final grand = catController.groupedCategories[child.id]![grandIndex];
+                                final selectedGrand = filController.selectedCategoryId.contains(grand.id);
+                                return ListTile(
+                                  shape: Border(),
+                                  title: Padding(
+                                    padding: const EdgeInsets.only(left: 32.0),
+                                    child: Row(
+                                      children: [
+                                        Checkbox(
+                                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                                          side: BorderSide(color: Colors.grey.shade300, width: 2),
+                                          value: selectedGrand,
+                                          onChanged: (v) => filController.toggleCategory(grand.id, v ?? false),
+                                          activeColor: AppColors.primary,
+                                        ),
+                                        Text(grand.name, style: AppTextsStyle.medium.copyWith(fontSize: 10)),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              }),
+                            )
+                          : ListTile(
+                              title: Row(
+                                children: [
+                                  Checkbox(
+                                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+                                    side: BorderSide(color: Colors.grey.shade300, width: 2),
+                                    value: selected,
+                                    onChanged: (v) => filController.toggleCategory(child.id, v ?? false),
+                                    activeColor: AppColors.primary,
+                                  ),
+                                  Text(child.name, style: AppTextsStyle.medium),
+                                ],
+                              ),
+                            );
+                    })
+                  : [],
+            );
+          });
+        },
+      ),
+
+      /*ListView.builder(
         itemCount: catController.categories.length,
         itemBuilder: (_, i) {
           final category = catController.categories[i];
@@ -60,7 +153,7 @@ class _CategoryPickerPageState extends State<CategoryPickerPage> {
             );
           });
         },
-      ),
+      ),*/
       bottomNavigationBar: BottomNavigationBarWidget(
         item: dummyProduct,
         title: AppTexts.apply,
