94abf8a69708d5fb37cf3eeac083b60d3b2a5277
PinzaruDaniel <pinzaru.danieel@gmail.com>
2025-09-04 15:30:56 +0300

    animarea la pagina cu import 'package:implicitly_animated_reorderable_list_2/implicitly_animated_reorderable_list_2.dart'; + flutter_animate

--- data/lib/mapper/product_mapper.dart
+++ data/lib/mapper/product_mapper.dart
@@ -1,9 +1,7 @@
 import 'package:data/mapper/category_mapper.dart';
 import 'package:data/mapper/specification_mapper.dart';
-import 'package:data/modules/products/models/remote/index.dart';
 import 'package:domain/modules/products/models/index.dart';
-
-import '../modules/products/models/local/index.dart';
+import '../modules/products/models/remote/index.dart';
 
 extension ProductApiDtoMapper on ProductApiDto {
   ProductEntity toEntity() {
@@ -20,72 +18,6 @@ extension ProductApiDtoMapper on ProductApiDto {
       specification: specification.map((e) => e.toEntity()).toList(),
       category: category?.expand((list) => list).map((e) => e.toEntity()).toList() ?? [],
       brand: brand,
-    );
-  }
-
-  ProductLocalDto toLocalDto() {
-    return ProductLocalDto(
-      id: id,
-      name: name,
-      price: price,
-      discount: discount,
-      discountedPrice: discountedPrice,
-      imageUrl: imageUrl,
-      marks: marks,
-      stock: stock,
-      description: description,
-    );
-  }
-}
-
-extension ProductLocalDtoMapper on ProductLocalDto {
-  ProductEntity toEntity() {
-    return ProductEntity(
-      id: id,
-      name: name,
-      price: price,
-      discount: discount,
-      discountedPrice: discountedPrice,
-      imageUrl: imageUrl,
-      marks: marks,
-      stock: stock,
-      description: description,
-      specification: [],
-      category: [],
-      brand: null,
-    );
-  }
-
-  ProductApiDto toApiDto() {
-    return ProductApiDto(
-      id: id,
-      name: name,
-      price: price,
-      discount: discount,
-      discountedPrice: discountedPrice,
-      imageUrl: imageUrl,
-      marks: marks,
-      stock: stock,
-      description: description,
-      brand: null,
-      specification: null,
-      category: null,
-    );
-  }
-}
-
-extension ProductEntityMapper on ProductEntity {
-  ProductLocalDto toLocalDto() {
-    return ProductLocalDto(
-      id: id,
-      name: name,
-      price: price,
-      discount: discount,
-      discountedPrice: discountedPrice,
-      imageUrl: imageUrl,
-      marks: marks,
-      stock: stock,
-      description: description,
     );
   }
 }
--- presentation/lib/pages/delivery_address_page/delivery_address_page.dart
+++ presentation/lib/pages/delivery_address_page/delivery_address_page.dart
@@ -3,17 +3,16 @@ import 'package:get/get.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:implicitly_animated_reorderable_list_2/implicitly_animated_reorderable_list_2.dart';
 import 'package:implicitly_animated_reorderable_list_2/transitions.dart';
-
 import 'package:presentation/pages/delivery_address_page/widgets/delivery_type_widget.dart';
 import 'package:presentation/pages/delivery_address_page/widgets/selection_widget.dart';
 import 'package:presentation/util/widgets/text_field_widget.dart';
-import 'package:presentation/view/base_view_model.dart';
 import '../../controllers/controller_imports.dart';
 import '../../util/resources/app_colors.dart';
 import '../../util/resources/app_icons.dart';
 import '../../util/resources/app_texts.dart';
 import '../../util/widgets/app_bar_widget.dart';
 import '../../util/widgets/bottom_navigation_bar_widget.dart';
+import '../../view/base_view_model.dart';
 
 class DeliveryAddressPage extends StatefulWidget {
   const DeliveryAddressPage({super.key});
@@ -38,20 +37,59 @@ class _DeliveryAddressPageState extends State<DeliveryAddressPage> {
         ),
       ),
       body: SafeArea(
-        child: Obx(
-          () => Form(
-            key: _formKey,
-            child: ImplicitlyAnimatedReorderableList(
-              padding: const EdgeInsets.only(bottom: 10),
-              items: deliveryAddressController.allItems.toList(),
-              areItemsTheSame: (oldItem, newItem) => oldItem.hashCode == newItem.hashCode,
-              onReorderFinished: (item, from, to, newItems) {
+        child: Obx(() {
+          final items = deliveryAddressController.allItems.toList();
 
-                setState(() {
-                  deliveryAddressController.allItems.toList()..clear()..addAll(newItems);
-                });
+          return Form(
+            key: _formKey,
+            child: ImplicitlyAnimatedList<BaseViewModel>(
+              items: items,
+              padding: const EdgeInsets.only(bottom: 10),
+              shrinkWrap: true,
+              areItemsTheSame: (a, b) {
+                if (a.runtimeType != b.runtimeType) return false;
+                if (a is SelectionViewModel && b is SelectionViewModel) {
+                  return a.keyId == b.keyId;
+                } else if (a is TextFieldViewModel && b is TextFieldViewModel) {
+                  return a.keyId == b.keyId;
+                } else if (a is DeliveryTypeViewModel && b is DeliveryTypeViewModel) {
+                  return true;
+                }
+                return false;
               },
-              itemBuilder: (context, itemAnimation, item, index) {
+              removeItemBuilder: (context, animation, oldItem) {
+                Widget widget;
+                String keyValue;
+
+                if (oldItem is DeliveryTypeViewModel) {
+                  widget = DeliveryTypeWidget(itemViewModel: oldItem);
+                  keyValue = 'delivery_type';
+                } else if (oldItem is SelectionViewModel) {
+                  widget = SelectionWidget(itemViewModel: oldItem);
+                  keyValue = 'selection_${oldItem.keyId}';
+                } else if (oldItem is TextFieldViewModel) {
+                  widget = TextFieldWidget(itemViewModel: oldItem);
+                  keyValue = 'text_field_${oldItem.keyId}';
+                } else {
+                  widget = const SizedBox.shrink();
+                  keyValue = 'unknown_remove';
+                }
+
+                return AnimatedBuilder(
+                  animation: animation,
+                  builder: (context, child) {
+                    final fadeValue = animation.value;
+                    final scaleValue = 0.7 + 0.3 * fadeValue;
+
+                    return Opacity(
+                      opacity: fadeValue,
+                      child: Transform.scale(scale: scaleValue, child: child),
+                    );
+                  },
+                  child: KeyedSubtree(key: ValueKey(keyValue), child: widget),
+                );
+              },
+              itemBuilder: (context, animation, item, index) {
                 Widget widget;
                 String keyValue;
 
@@ -69,38 +107,50 @@ class _DeliveryAddressPageState extends State<DeliveryAddressPage> {
                   keyValue = 'unknown_$index';
                 }
 
-                return Reorderable(
-                  key: ValueKey(keyValue),
-                  builder: (context, dragAnimation, inDrag) {
-                    return SizeFadeTransition(
-                      sizeFraction: 0.7,
-                      curve: Curves.easeInOut,
-                      animation: itemAnimation,
-                      child: item is DeliveryTypeViewModel
-                          ? widget
-                          : widget,
-                    );
-                  },
+                return SizeFadeTransition(
+                  animation: animation,
+                  child: KeyedSubtree(
+                    key: ValueKey(keyValue),
+                    child: item is DeliveryTypeViewModel
+                        ? widget
+                        : widget
+                              .animate()
+                              .fadeIn(duration: 500.ms, delay: (50 * index).ms)
+                              .slideY(
+                                begin: 0.8,
+                                end: 0.0,
+                                duration: 500.ms,
+                                delay: (100 * index).ms,
+                                curve: Curves.easeInOut,
+                              )
+                              .scaleXY(
+                                begin: 0.7,
+                                end: 1,
+                                duration: 500.ms,
+                                delay: (75 * index).ms,
+                                curve: Curves.easeInOut,
+                              ),
+                  ),
                 );
               },
             ),
-          ),
-        ),
+          );
+        }),
       ),
+
       bottomNavigationBar: BottomNavigationBarWidget(
-        title:  AppTexts.save,
+        title: AppTexts.save,
         showIcon: false,
         onTap: () {
           if (_formKey.currentState?.validate() ?? false) {
             checkoutController.initAllItems();
-            deliveryAddressController.initItems();
+            deliveryAddressController.onInit();
             Navigator.pop(context);
           }
         },
         titleDialog: AppTexts.oops,
         contentDialog: AppTexts.noProductsToShow,
       ),
-
     );
   }
 }
