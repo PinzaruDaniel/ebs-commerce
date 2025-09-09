b27723538f61330c01f6262287d89b046d984799
PinzaruDaniel <pinzaru.danieel@gmail.com>
2025-09-09 16:01:25 +0300

    animatii in mai multe locuri

--- presentation/lib/view/country_view_model.dart
+++ presentation/lib/view/country_view_model.dart
@@ -2,6 +2,7 @@
 //instead of string to use extended viewmodel
 //function from viewmodel to widget
 //Select City. Validation on delivery address, with placeholders
+
 class CountryViewModel {
   final String name;
   final String iso2;
