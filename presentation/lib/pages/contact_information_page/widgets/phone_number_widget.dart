5eaa6d358177ca079609052ba8f7faca40cbeae8
PinzaruDaniel <pinzaru.danieel@gmail.com>
2025-10-13 16:47:01 +0300

    added DropDown Button 2 and modify the design

--- presentation/lib/pages/contact_information_page/widgets/phone_number_widget.dart
+++ presentation/lib/pages/contact_information_page/widgets/phone_number_widget.dart
@@ -3,6 +3,8 @@ import 'package:presentation/view/base_view_model.dart';
 import 'package:presentation/view/dial_codes_view_model.dart';
 import 'package:presentation/view/flag_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:dropdown_button2/dropdown_button2.dart';
+
 
 class PhoneNumberViewModel extends BaseViewModel {
   final List<DialCodesViewModel> dialCodes;
@@ -36,7 +38,7 @@ class _PhoneNumberWidgetState extends State<PhoneNumberWidget> {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(widget.itemViewModel.title),
-        SizedBox(height: 4,),
+        SizedBox(height: 4),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
           mainAxisSize: MainAxisSize.min,
@@ -47,34 +49,39 @@ class _PhoneNumberWidgetState extends State<PhoneNumberWidget> {
                 border: Border.all(color: Colors.grey.shade300),
                 borderRadius: BorderRadius.circular(5),
               ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8),
-                child: DropdownButton<DialCodesViewModel>(
-                  iconSize: 20,
-                  value: widget.itemViewModel.dialCodes.contains(widget.itemViewModel.selectedDialCode)
-                      ? widget.itemViewModel.selectedDialCode
-                      : widget.itemViewModel.dialCodes.first,
-                  underline: const SizedBox(),
-                  onChanged: (value) {
-                    if (value != null) {
-                      setState(() {
-                        widget.itemViewModel.selectedDialCode = value;
-                      });
-                    }
-                  },
-                  items: widget.itemViewModel.dialCodes.map((code) {
-                    return DropdownMenuItem<DialCodesViewModel>(
-                      value: code,
-                      child: Text('${_getUnicodeFlag(code.code)} ${code.dialCode}'),
-                    );
-                  }).toList(),
+              child: DropdownButton2(
+                underline: Container(),
+                items: widget.itemViewModel.dialCodes.map((code) {
+                  return DropdownMenuItem<DialCodesViewModel>(
+                    value: code,
+                    child: Text(
+                      '${_getUnicodeFlag(code.code)} ${code.name.length > 10 ? '${code.name.substring(0, 10)}…' : code.name} ${code.dialCode}',
+                        overflow: TextOverflow.ellipsis,
+
+                    ),
+                  );
+                }).toList(),
+                value: widget.itemViewModel.selectedDialCode,
+                onChanged: (value) {
+                  if (value != null) {
+                    setState(() {
+                      widget.itemViewModel.selectedDialCode = value;
+                    });
+                  }
+                },
+                dropdownStyleData: DropdownStyleData(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.grey.shade300, width: 2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
                 ),
               ),
             ),
 
             const SizedBox(width: 8),
 
-            Expanded(child: TextFieldWidget(itemViewModel: widget.itemViewModel.textFieldViewModel, showTitle: false,)),
+            Expanded(child: TextFieldWidget(itemViewModel: widget.itemViewModel.textFieldViewModel, showTitle: false)),
           ],
         ),
       ],
